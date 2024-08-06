96657938a4b2fcb16858a98f835489fd882a0d6b
Traveliki- <https://github.com/duriskifeb>
2024-08-06 21:56:58 +0700

    chenge number file to java

--- Perkuliahan/Latiahan/Number/Number.java
+++ Perkuliahan/Latiahan/Number/Number.java
@@ -33,7 +33,20 @@ public class Number {
         //This is a pictrue a number three 
         for (int i = 1; i <= 7; i++) {
             for (int j = 1; j <= 5; j++) {
-                if (i == 1 || i == 4 || i == 7 || (j == 5 && i <= 4) || (j == 1 && i >= 4)) {
+                if (i == 1 || i == 4 || i == 7 || j == 5 || (j == 0 && (i == 2 || i == 6))) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            
+        }
+        System.out.println("");
+        //This is a pictrue a number four 
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if ((j == 1 && i <= 4)|| (j == 5 && i >= 1) || (i == 4)) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
---
+++ Perkuliahan/Latihan/Number/teskah.java
@@ -0,0 +1,16 @@
+package Latihan.Number;
+
+public class teskah {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 4 || i == 7 || j == 5 || (j == 0 && (i == 2 || i == 6))) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
