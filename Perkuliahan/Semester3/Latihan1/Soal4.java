ef4a931777e6effcc7f69885aee21c3ba7eb5a64
Traveliki- <https://github.com/duriskifeb>
2024-09-20 23:55:02 +0700

    Done tugas, yang nomer 4 masih ambigu : )

--- Perkuliahan/Semester3/Latihan1/Soal4.java
+++ Perkuliahan/Semester3/Latihan1/Soal4.java
@@ -1,5 +1,33 @@
 package Semester3.Latihan1;
 
+import java.util.Scanner;
+
 public class Soal4 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter a positive integer (or 0 to exit): ");
+            int num = scanner.nextInt();
+            if (num == 0) {
+                break; // sentinel value, exit loop
+            }
+
+            if (isPerfect(num)) {
+                System.out.println("perfect");
+            } else {
+                System.out.println("not perfect");
+            }
+        }
+    }
+
+    public static boolean isPerfect(int num) {
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
 }
