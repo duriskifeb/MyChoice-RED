8a0bf2dd9da8e46d50e8b70f9337fb382f63bf0d
Traveliki- <https://github.com/duriskifeb>
2024-12-17 11:31:35 +0700

    progres modedl tubas tubes

---
+++ TubesStrukdatList/src/Data/Model/TransaksiRumah.java
@@ -0,0 +1,10 @@
+package Data.Model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class TransaksiRumah {
+    private String noTransaksi;
+    private Date tanggalTransaksi;
+
+}
